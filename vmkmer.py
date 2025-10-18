488890ab07ba9258b08eb984547d628e0ca495d7
Ahmed Lamloum <42451526+a-lamloum@users.noreply.github.com>
2025-10-18 15:30:28 +0200

    Remove GitHub link from vmkmer.py
    Removed GitHub link from the copyright section.

--- vmkmer.py
+++ vmkmer.py
@@ -1,6 +1,5 @@
 """
 Copyright 2020 | Ahmed Lamloum, Asmaa Ali, Mohamed AboelEla, and Usama Bakry
-https://github.com/ubakry/VMK-mer
 
 This file is used for running VMK-mer directly without installing it.
 
